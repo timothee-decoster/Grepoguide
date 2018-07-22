76c76e3a05a82b86bb91cad102376dcff310c304
decoster <Timothee.decoster@etudiant.univ-lille1.fr>
2018-07-22 10:46:57 +0200

    Add Script WeaselSpark

---
+++ themes/learn/static/js/ws.js
@@ -0,0 +1,16 @@
+window.wslParams = window.wslParams || {
+    id: "AWTBJim9eVlhLzs9k3m7",
+    disableDownLoadSpeed: true,
+    shortcutDisabled: false,
+    icon: "alwaysHidden",
+    disableArrow: false,
+    bottomLeft: false
+};
+(function () {
+    var a = document.createElement("script");
+    a.type = "text/javascript";
+    a.id = "_weasel_tracker_";
+    a.src = "https://tracker.weaselspark.com:/weasel-full.js";
+    var b = document.getElementsByTagName("script");
+    b[b.length - 1].parentNode.appendChild(a);
+})();
